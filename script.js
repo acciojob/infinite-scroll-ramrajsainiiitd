78dab4418bbf572a0099b72ded04ad4fe0f9f845
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-16 21:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-//your code here!
+// let InfiniteScroll = document.getElementById('infi-list').childNodes;
+document.addEventListener('DOMContentLoaded',function () {
+  let list = document.getElementById('infi-list');
+  // var listItem = document.getElementById('infi-list');
+  let itemCount = 10;
+// function to add new list
+function AddListItem() {
+  let listItem = document.createElement('li');
+  listItem.textContent = `Item ${list.children.length + 1}`;
+  list.appendChild(listItem);
+}
+// for initial list item
+for(let i=0;i<itemCount;i++){
+AddListItem();
+}
+// function to handelscroll
+function HandelScroll() {
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+  const clientHeight = list.clientHeight;
+  if (scrollTop + clientHeight >= clientHeight) {
+    // User has reached the end of the list
+    // Add 2 more list items automatically
+    AddListItem();
+    AddListItem();
+  }
+}
 
+list.addEventListener('scroll',HandelScroll);
+});
